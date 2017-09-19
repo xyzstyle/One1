36078fa1ee1d6aad4dc6653e65ff957a4068aa66
xyz <xyzstyle@163.com>
2017-09-19 22:04:22 +0800

    version3

--- src/com/xyz/test/My.java
+++ src/com/xyz/test/My.java
@@ -4,5 +4,6 @@ public class My {
     public static void main(String[] args){
         System.out.println("first");
         System.out.println("update my function");
+        System.out.println("v3");
     }
 }
