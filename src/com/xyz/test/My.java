2a95d1e3f6132403a0b1c8b63143abf037245183
xyz <xyzstyle@163.com>
2017-09-19 21:56:28 +0800

    version2

--- src/com/xyz/test/My.java
+++ src/com/xyz/test/My.java
@@ -3,5 +3,6 @@ package com.xyz.test;
 public class My {
     public static void main(String[] args){
         System.out.println("first");
+        System.out.println("update my function");
     }
 }
